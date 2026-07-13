091fb73ca044279fa1f2f145ab998ecb3541a2ab
Amezande <oscarabrjs@gmail.com>
2026-07-13 12:43:22 -0600

    Add examen.sh to automate project setup and logging
    Create a script to set up the final project and log emotion detection results.

---
+++ examen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir final_project
+cd final_project
+git clone https://github.com/Oscar-Amacende/oaqjp-final-project-emb-ai
+#touch emotion_detection.py
+echo "import json import requests #Funcion de detección de emociones def emotion_detector(text_to_anal>
+echo "import json import requests #Funcion de detección de emociones def emotion_detector(text_to_anal>
+
+
+#Importamos la aplicacion
+python3 -c "from emotion_detection import emotion_detector"
+python3 -c "from emotion_detection import emotion_detector" >2b_application_creation.txt
+#Logs de importacion
+python3 -c "resultado = emotion_detector("I love this new techmology")"
+python3 -c "resultado = emotion_detector("I love this new techmology")" >>2b_application_creation.txt
