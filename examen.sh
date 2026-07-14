d03b799604780e3faf3d4226c9b699632130fc38
Amezande <oscarabrjs@gmail.com>
2026-07-14 09:33:02 -0600

    Enhance examen.sh for directory creation and cloning
    Updated script to create final_project directory if it doesn't exist and clone the repository only if it hasn't been cloned yet. Modified Python commands for better output handling.

--- examen.sh
+++ examen.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mkdir final_project
+mkdir -p final_project
 cd final_project
-git clone https://github.com/Oscar-Amacende/oaqjp-final-project-emb-ai
+
+if [ ! -d "oaqjp-final-project-emb-ai" ]; then
+        git clone https://github.com/Oscar-Amacende/oaqjp-final-project-emb-ai
+fi
+
 #touch emotion_detection.py
 cat << 'EOF' > emotion_detection.py
 import requests
@@ -37,9 +41,12 @@ EOF
 #Copiamos al otro archivo
 cp emotion_detection.py 2a_emotion_detection
 
+#importamos lo necesario de python
+python3 -m pip install requests
+
 #Importamos la aplicacion
-python3 -c "from emotion_detection import emotion_detector"
-python3 -c "from emotion_detection import emotion_detector" >2b_application_creation.txt
+python3 -c 'from emotion_detection import emotion_detector; print(emotion_detector("I love this new technology"))' > 2b_application_creation
+#python3 -c "from emotion_detection import emotion_detector" >2b_application_creation.txt
 #Logs de importacion
-python3 -c "resultado = emotion_detector("I love this new techmology")"
-python3 -c "resultado = emotion_detector("I love this new techmology")" >>2b_application_creation.txt
+#python3 -c 'resultado = emotion_detector("I love this new technology")'
+#python3 -c 'resultado = emotion_detector("I love this new technology")' >>2b_application_creation.txt
